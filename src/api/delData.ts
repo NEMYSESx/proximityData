52d83fad9aba1e03d2444685b04a4949cc90ef62
NEMYSESx <shakirraza4227@gmail.com>
2024-08-17 04:31:34 +0530

    added del api

--- src/api/delData.ts
+++ src/api/delData.ts
@@ -2,7 +2,7 @@ import { db } from "../services/db";
 import { Request, Response } from "express";
 
 export const delData = async (req: Request, res: Response) => {
-  const { socketId } = req.body;
+  const { socketId } = req.body as { socketId: string };
 
   if (!socketId) {
     return res.status(400).json({ error: "socketId is required" });
