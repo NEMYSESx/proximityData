ff966c2a604e8ac318bbb3346cc92ec796269cdd
NEMYSESx <shakirraza4227@gmail.com>
2024-08-17 04:29:35 +0530

    added del api

---
+++ src/api/delData.ts
@@ -0,0 +1,22 @@
+import { db } from "../services/db";
+import { Request, Response } from "express";
+
+export const delData = async (req: Request, res: Response) => {
+  const { socketId } = req.body;
+
+  if (!socketId) {
+    return res.status(400).json({ error: "socketId is required" });
+  }
+
+  try {
+    const result = await db.user.delete({
+      where: {
+        socketId: socketId,
+      },
+    });
+    res.status(200).json(result);
+  } catch (error) {
+    console.error("Error deleting the data", error);
+    res.status(500).json({ error: "Error deleting the data" });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { getData } from "./api/getData";
 import { putData } from "./api/putData";
+import { delData } from "./api/delData";
 const app = express();
 app.use(
   cors({
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.post("/getData", getData);
 app.post("/putData", putData);
+app.post("/delData", delData);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
